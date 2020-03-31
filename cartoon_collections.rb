a322ee9be275c04f01b14a3b1a14b1dd2e927474
Sonja Parsell <hello@sparsell.com>
2020-03-31 12:26:04 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarf_by_number = []
+  dwarves.each_with_index do |dwarf, index|
+    dwarf_by_number << ("#{index + 1}. #{dwarf}")
+  end
+    puts dwarf_by_number
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(array)
+  array.collect {|word| word.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(array)
+if array.any? {|word| word.length > 4}
+  true
+elsif
+  array.all? {|word| word.length <= 4}
+  false
+else
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+array = ["salt", "bread", "apples", "gouda", "brie", "almonds"]
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|item| cheese_types.include? (item)}
 end
